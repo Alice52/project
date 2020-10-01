f8fe6c81c53ffc900a303743cefba951421fb7ed
Alice52 <zzhang_xz@163.com>
2020-10-01 15:03:38 +0800

    feature: add ec-admin front project by fast-renren-vue project

---
+++ frontend/ec-admin/src/views/common/home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="mod-home">
+    <h3>项目介绍</h3>
+    此管理后台是基于 人人开源 改的
+  </div>
+</template>
+
+<script>
+  export default {
+  }
+</script>
+
+<style>
+  .mod-home {
+    line-height: 1.5;
+  }
+</style>
+
--- frontend/i18n/en-US.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable max-len */
-export default {
-  i18n: {
-    title: 'Internationalization Demo',
-    currentTime: 'Current Datetime: {datetime}',
-  },
-};
